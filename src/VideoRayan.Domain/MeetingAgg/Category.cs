ea973aa6341521a229fc7dfc8e46c95c4c60912b
hamidba <balalzadehhamid79@gmail.com>
2022-07-12 13:50:28 +0430

    Add Category And Its Repository  Contract

---
+++ src/VideoRayan.Domain/MeetingAgg/Category.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Framework.Domain;
+
+namespace VideoRayan.Domain.MeetingAgg
+{
+	public class Category : EntityBase
+	{
+        public string Title { get;private set; }
+        public string Description { get; private set; }
+
+        public Category(string title, string description)
+        {
+            Title = title;
+            Description = description;
+        }
+
+        public void Edit(string title, string description)
+        {
+            Title = title;
+            Description = description;
+        }
+    }
+}
---
+++ src/VideoRayan.Domain/MeetingAgg/Repositories /ICategoryRepository.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using Framework.Domain;
+
+namespace VideoRayan.Domain.MeetingAgg.Repositories
+{
+	public interface ICategoryRepository : IRepository<Category>
+	{
+	}
+}
