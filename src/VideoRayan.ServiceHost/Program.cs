e7950b1c6c1b5ac1319d7f49bcf3a14b1ce25f6d
Hamid Balalzadeh <balalzadehhamid79@gmail.com>
2022-07-19 11:20:57 +0430

    Add Endpoint Layer , Configure Jwt Setting

---
+++ src/VideoRayan.ServiceHost/Program.cs
@@ -0,0 +1,58 @@
+using Framework.Api.Jwt;
+using Framework.Application.Hashing;
+using Framework.Application.Sms;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using VideoRayan.Infrastructure.Configuration;
+
+var builder = WebApplication.CreateBuilder(args);
+var service = builder.Services;
+// Add services to the container.
+
+service.AddHttpContextAccessor();
+service.AddTransient<IJwtHelper, JwtHelper>();
+service.AddTransient<ISmsService, SmsService>();
+service.AddTransient<IPasswordHasher, PasswordHasher>();
+VideoRayanBootstrapper.Configure(service, builder.Configuration.GetConnectionString("Ir-Cafe"));
+
+service.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(options =>
+{
+    options.SaveToken = true;
+    options.RequireHttpsMetadata = false;
+    options.TokenValidationParameters = new TokenValidationParameters()
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidAudience = builder.Configuration["JWT:ValidAudience"],
+        ValidIssuer = builder.Configuration["JWT:ValidIssuer"],
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["JWT:Secret"]))
+    };
+});
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
