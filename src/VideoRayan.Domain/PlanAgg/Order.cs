455fe6998c7ebb469b62edc4371c907180fce8e5
Hamid Balalzadeh <balalzadehhamid79@gmail.com>
2022-07-19 09:38:21 +0430

    Add PlanAgg Domain

---
+++ src/VideoRayan.Domain/PlanAgg/Order.cs
@@ -0,0 +1,33 @@
+﻿using Framework.Domain;
+using VideoRayan.Domain.CustomerAgg;
+
+namespace VideoRayan.Domain.PlanAgg
+{
+    public class Order : EntityBase
+    {
+        public Guid CustomerId { get; private set; }
+        public Guid PlanId { get; private set; }
+        public double PayAmount { get; private set; }
+        public string MobileNumber { get; set; }
+        public long RefId { get; private set; }
+        public bool IsPayed { get; private set; }
+
+        public Plan Plan { get; private set; }
+        public Customer Customer { get; private set; }
+
+        public Order(Guid customerId, Guid planId, double payAmount, string mobileNumber)
+        {
+            CustomerId = customerId;
+            PlanId = planId;
+            PayAmount = payAmount;
+            MobileNumber = mobileNumber;
+            IsPayed = false;
+        }
+
+        public void PaymentSucceeded(long refId)
+        {
+            if (refId > 0) RefId = refId;
+            IsPayed = true;
+        }
+    }
+}
---
+++ src/VideoRayan.Domain/PlanAgg/Plan.cs
@@ -0,0 +1,46 @@
+﻿using Framework.Domain;
+
+namespace VideoRayan.Domain.PlanAgg
+{
+    public class Plan : EntityBase
+    {
+        public string Title { get; private set; }
+        public int PeriodPerDay { get; private set; }
+        public string ImageName { get; private set; }
+        public double Cost { get; private set; }
+        public string Description { get; private set; }
+        public string Ps { get; private set; }
+        public long OrderCount { get; private set; }
+
+        public List<Order> Orders { get; private set; }
+
+        public Plan(string title, int periodPerDay, string imageName, double cost, string description, string ps)
+        {
+            Title = title;
+            PeriodPerDay = periodPerDay;
+            ImageName = imageName;
+            Cost = cost;
+            Description = description;
+            Ps = ps;
+            OrderCount = 0;
+        }
+
+        public void Edit(string title, int periodPerDay, string imageName, double cost, string description, string ps)
+        {
+            Title = title;
+            PeriodPerDay = periodPerDay;
+
+            if (!string.IsNullOrWhiteSpace(imageName))
+                ImageName = imageName;
+
+            Cost = cost;
+            Description = description;
+            Ps = ps;
+
+            LastUpdateDate = DateTime.Now;
+        }
+
+        public long AddOrder() => ++OrderCount;
+
+    }
+}
