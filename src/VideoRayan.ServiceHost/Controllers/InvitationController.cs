f79756b6659334cfb64d6febe7975aa80b4fd8f8
Hamid Balalzadeh <balalzadehhamid79@gmail.com>
2022-07-23 09:22:01 +0430

    Add Make Invitation To Meeting For Audience

---
+++ src/VideoRayan.ServiceHost/Controllers/InvitationController.cs
@@ -0,0 +1,30 @@
+﻿using Framework.Api;
+using Microsoft.AspNetCore.Mvc;
+using VideoRayan.Application.Contract.MeetingAgg;
+using VideoRayan.Application.Contract.MeetingAgg.Contracts;
+
+namespace VideoRayan.ServiceHost.Controllers
+{
+    public class InvitationController : ApiBaseController
+    {
+        private readonly IAudienceMeetingApplication _audienceMeetingApplication;
+
+        public InvitationController(IAudienceMeetingApplication audienceMeetingApplication) => _audienceMeetingApplication = audienceMeetingApplication;
+
+        [HttpPost]
+        public async Task<IActionResult> InviteToMeeting([FromBody] AudienceMeetingDto command)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    var result = await _audienceMeetingApplication.AddAudiencesToMeeting(command);
+                    return result.IsSucceeded ? Ok(result.Message) : BadRequest(result.Message);
+                }
+
+                return BadRequest(ApiResultMessages.ModelStateNotValid);
+            }
+            catch (Exception e) { return BadRequest(e.InnerException!.Message); }
+        }
+    }
+}
